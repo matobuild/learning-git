e03c58b61d94f3d04041b8b8c85c020845bbd83e
Your mato <you@example.com>
2020-03-07 19:44:36 +0800

    Initial Commit

---
+++ learning git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learning git
+//
+//  Created by kittawat phuangsombat on 2020/3/7.
+//  Copyright © 2020 kittawat phuangsombat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
